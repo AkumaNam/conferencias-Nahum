9931eea6ef904d332fa9f0ae52f6c826858841bd
akumanam <nahumlop17@gmail.com>
2021-06-11 16:17:34 -0600

    tarea 2 Conferencistas

--- app_registro/admin.py
+++ app_registro/admin.py
@@ -1,21 +1,15 @@
 from django.contrib import admin
 from django.contrib.admin.decorators import register
 from app_registro.models import Conferencia, Conferencista, Participantes
-# Register your models here.
+
+#------------------------------------------------------------------------------------------
 class ConferenciaAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre', 'fecha', 'hora', 'conferencista')
     list_editable = ('nombre', 'conferencista')
 
 
-
-
 admin.site.register(Conferencia, ConferenciaAdmin)
-
-
-
-
+# ------------------------------------------------------------------------
 admin.site.register(Conferencista)
-
-
-
+# ------------------------------------------------------------------------
 admin.site.register(Participantes)
--- app_registro/views.py
+++ app_registro/views.py
@@ -1,57 +1,155 @@
-
-from app_registro.models import Participantes
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from django.contrib import messages
+from django.urls import reverse
+from .models import Participantes
+from .models import Conferencista
+
+from telegram import Bot # pip install python-telegram-bot
 
 
+TOKEN = '1722646107:AAFETIXdzMbRjHqBGAYQEstprLq8D4yBwJs'
+GROUP_ID = -502330311
 
-# Create your views here.
+bot = Bot(token=TOKEN)
+
 def index(request):
-     return render(request, 'registro/index.html')
+    return render(request, 'registro/index.html')
 
 
-def participantes(request): 
+def participantes(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        apellido = request.POST.get('apellido')
+        correo = request.POST.get('correo')
+        twitter = request.POST.get('twitter')
 
-     
+        p = Participantes(nombre=nombre, apellido=apellido, correo=correo, twitter=twitter)
+        p.save()
 
-     if request.method == 'POST':
-          nombre = request.POST.get('nombre')
-          apellido = request.POST.get('apellido')
-          correo = request.POST.get('correo')
-          twitter = request.POST.get('twitter')
+        msj = f'El participante {nombre} {apellido} ha sido registrado con éxito.'
 
-          p = Participantes(nombre = nombre, apellido = apellido, correo = correo, twitter = twitter)
-          p.save()
-          
-          messages.add_message(request, messages.INFO, 'El participante {nombre} {apellido} ha sido registrado con exito')
-          '''
-          return JsonResponse({
-               'nombre': nombre,
-               'apellido': apellido,
-               'correo': correo,
-               'twitter': twitter,
-               'Ok': True,
-               'msj' : 'El participante ha sido registrado con exito'
-          })
-          '''
+        # Codigo para enviar un mensaje a un grupo de telegram
 
-          #ctx = {
-           
-          #    'participantes':  Participantes.objects.all().order_by('nombre')
-          #}
-          
-          #return HttpResponse('El participante ha sido registrado')
-          #return render(request, 'registro/participantes.html', ctx)
-     
-     #Metodo GET, PUT, PATVH, DELETE
+        try:
+            bot.send_message(chat_id=GROUP_ID, text=msj)
+        except Exception as e:
+            msj += f'<br/><strong>{e}</strong>'
+        
+        messages.add_message(request, messages.INFO, msj)
 
-     #la primera consulta: select * from participantes
-     #realizar un Queryset con el ORM de DJANGO
-     data = Participantes.objects.all().order_by('nombre')
+        # return JsonResponse({
+        #     'nombre': nombre,
+        #     'apellido': apellido,
+        #     'correo': correo,
+        #     'twitter': twitter,
+        #     'OK': True,
+        #     'msj': 'El participante ha sido registrado con éxito'
+        # })
 
-     ctx = {
-          'participantes': data
-     }
-          
-     return render(request, 'registro/participantes.html', ctx)
+        # ctx = {
+        #     'participantes': Participante.objects.all().order_by('nombre')
+        # }
+
+        # return HttpResponse('El participante ha sido registrado')
+        # return render(request, 'registro/participantes.html', ctx)
+    
+    # Metodo GET, PUT, PATCH, DELETE
+
+    # La primera consulta: select * from participantes order by nombre desc
+    # Realizar un Queryset con el ORM de Django
+    activo = 'participantes'
+    q = request.GET.get('q')
+
+    if q:
+        data = Participantes.objects.filter(nombre__startswith=q).order_by('nombre')
+
+        '''
+            select * 
+            from participantes
+            where nombre like 'n%'
+        '''
+    else:
+        data = Participantes.objects.all().order_by('nombre')
+
+    ctx = {
+        'activo': activo,
+        'participantes': data,
+        'q': q
+    }
+
+    return render(request, 'registro/participantes.html', ctx)
+
+
+def eliminar_participante(request, id):
+    Participantes.objects.get(pk=id).delete()
+    return redirect(reverse('participantes'))
+
+
+def editar_participante(request, id):
+    par = get_object_or_404(Participantes, pk=id)
+
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        apellido = request.POST.get('apellido')
+        correo = request.POST.get('correo')
+        twitter = request.POST.get('twitter')
+
+        par.nombre = nombre
+        par.apellido = apellido
+        par.correo = correo
+        par.twitter = twitter
+        par.save()
+
+    #par = Participante.objects.get(pk=id)    
+    data = Participantes.objects.all().order_by('nombre')
+
+    ctx = {
+        'activo': 'participantes',
+        'participantes': data,
+        'p': par
+    }
+
+    return render(request, 'registro/participantes.html', ctx)
+
+
+def conferencistas(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        apellido = request.POST.get('apellido')
+        experiencia = request.POST.get('experiencia')
+
+        c = Conferencista(nombre=nombre, apellido=apellido, experiencia=experiencia)
+        c.save()
+
+        msj = f'El conferencista {nombre} {apellido} ha sido registrado con éxito.'
+
+        try:
+            bot.send_message(chat_id=GROUP_ID, text=msj)
+        except Exception as e:
+            msj += f'<br/><strong>{e}</strong>'
+        
+        messages.add_message(request, messages.INFO, msj)
+
+    activo = 'conferencistas'
+    q = request.GET.get('q')
+
+    if q:
+        data = Conferencista.objects.filter(nombre__startswith=q).order_by('nombre')
+
+        '''
+            select * 
+            from conferencistas
+            where nombre like 'n%'
+        '''
+    else:
+        data = Conferencista.objects.all().order_by('nombre')
+
+    ctx = {
+        'activo': activo,
+        'conferencistas': data,
+        'q': q
+    }
+    return render(request, 'registro/conferencistas.html', ctx)
+
+
